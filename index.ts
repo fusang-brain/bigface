796cba331490e93413a1ec7a3dfb7c3377fb8239
joson <joson.chou@outlook.com>
2019-10-03 11:08:23 +0800

    feat: add hash function

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
+import * as _hash from './lib/hash';
 export { default as fs }  from 'mz/fs';
 export { default as pump } from './pump';
+export { default as crypto } from 'crypto';
+export const hash = _hash;
---
+++ lib/hash.ts
@@ -0,0 +1,59 @@
+import crypto, { BinaryLike, HexBase64Latin1Encoding } from 'crypto';
+import { fs } from 'mz';
+
+export function sha1(data: BinaryLike, digest: HexBase64Latin1Encoding) {
+  var hash = crypto.createHash('sha1');
+  hash.update(data);
+  if (!digest) {
+    digest = 'hex';
+  }
+
+  return hash.digest(digest);
+}
+
+export function md5(data: BinaryLike, digest: HexBase64Latin1Encoding) {
+  var hash = crypto.createHash('md5');
+  hash.update(data);
+  if (!digest) {
+    digest = 'hex';
+  }
+
+  return hash.digest(digest);
+}
+
+export function md5File(path: string) {
+  const getMd5 = (path: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      try {
+        //从文件创建一个可读流
+        // const stat = await fs.stat(path);
+  
+        const stream = fs.createReadStream(path);
+        
+        // const fsHash = _crypto.createHash('md5');
+        const fsHash = crypto.createHash('md5');
+  
+        stream.on('data', function(d) {
+            fsHash.update(d);
+        });
+  
+        stream.on('end', function() {
+            const md5 = fsHash.digest('hex');
+            resolve(md5);
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }; 
+  
+  const stat = await fs.stat(path);
+
+  if (!stat.isFile()) {
+    throw new Error('Not found this file');
+  }
+
+  return getMd5(path);
+}
+
+// export default crypto;
